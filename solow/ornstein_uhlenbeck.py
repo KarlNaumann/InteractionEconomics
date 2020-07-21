8eee09adacba00b03fb6eb934a2edf5490b2ca2b
KarlNaumann <karl.naumann@icloud.com>
2020-07-21 14:27:27 +0200

    Cleanup

--- solow/ornstein_uhlenbeck.py
+++ solow/ornstein_uhlenbeck.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class OrnsteinUhlenbeck(object):
-    def __init__(self, decay: float, drift: float, diffusion: float):
+    def __init__(self, decay: float, drift: float, diffusion: float,
+                 t0: float = 0):
         """ Class implementing the Ornstein-Uhlenbeck process
 
             dxt = decay * (drift - xt) * dt + diffusion * dWt
@@ -17,6 +18,8 @@ class OrnsteinUhlenbeck(object):
             asymptotic mean of the process
         diffusion   :   float
             diffusion coefficient
+        t0  :   float
+            starting time for the process
 
         Public Attributes
         -----------------
@@ -36,20 +39,18 @@ class OrnsteinUhlenbeck(object):
         self.drift = drift
         self.diffusion = diffusion
 
-        self.history = []
-        self.intervals = []
+        self.history = [self.drift]
+        self.points = [t0]
 
-    def euler_maruyama(self, prior: float, delta: float, rand: float = None):
+    def euler_maruyama(self, t: float, rand: float = None):
         """ Numerical approximation of the Ornstein-Uhlenbeck process by means
         of the Euler-Maruyama scheme:
         xt+1 = xt + b(xt)*delta_t + diff*sqrt(delta_t)*norm_rand
 
         Parameters
         ----------
-        prior   :   float
-            prior realisation of the process
-        delta    :   float
-            interval delta_t over which the process develops
+        t    :   float
+            time t where the next evaluation occurs
         rand    :   float
             optionally pass an externally generated random N(0,1) variable
 
@@ -61,7 +62,14 @@ class OrnsteinUhlenbeck(object):
         # Generate a N(0,1) random variable if not provided yet
         rand = rand if rand is not None else np.random.normal(0, 1)
 
+        # Determine the interval
+        delta = t - self.points[-1]
+        self.points.append(t)
+
+        # Next realisation as function of prior realisation
+        prior = self.history[-1]
         b_xt = self.decay * (self.drift - prior)
         x_next = prior + delta * b_xt + self.diffusion * np.sqrt(delta) * rand
+        self.history.append(x_next)
 
         return x_next
