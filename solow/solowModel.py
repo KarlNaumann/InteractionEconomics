03a5a5495592296401581a70e9a6bf99a3127da2
KarlNaumann <karl.naumann@icloud.com>
2020-08-17 15:32:13 +0200

    Recession indicators and file cleanup

--- solow/solowModel.py
+++ solow/solowModel.py
@@ -36,116 +36,12 @@ class SolowModel(object):
 
         # Storage
         self.path = None
+        self.recessions = None
 
-    def solve(self, start: list, t0: float = 0, t_end: float = 1e2,
-              stoch: bool = True, theta: float = 0.2, sigma: float = 1.0,
-              y_args: dict = None, cap_args: dict = None):
-        """ Iterate through the Solow model to generate a path for output,
-        capital (supply & demand), and sentiment
-
-        Parameters
-        ----------
-        start       :   list
-            initial values, order = y, ks, kd, s, h, r
-        t0, t_end   :   float
-            integration interval
-        stoch       :   bool
-            whether to solve the system with a stochastic parameter
-        theta, sigma:   float
-            decay and variance of the Ornstein-Uhlenbeck process
-        y_args      :   dict
-            (optional) dictionary of production arguments (incl. tech0, rho, e,
-            tau_y, dep)
-        cap_args    :   dict
-            (optional) dictionary of capital markets arguments (incl. saving,
-            tau_h, tau_s, c1, c2, beta1, beta2, gamma, h_h)
-
-        Returns
-        -------
-
-        """
-
-        # Arguments
-        t_eval = np.arange(1, int(t_end) - 1)
-        gamma, h_h = cap_args['gamma'], cap_args['h_h']
-
-        if y_args is None or cap_args is None:
-            ou = self.ou
-        else:
-            ou = OrnsteinUhlenbeck(decay=theta, diffusion=sigma)
-
-        if stoch:
-            args = (y_args, cap_args, gamma, 10, h_h, ou)
-        else:
-            args = (y_args, cap_args, gamma, 10, h_h, None)
-
-        # Generate path of variables
-        path = solve_ivp(self._log_step, t_span=(1, t_end), y0=start,
-                         method='RK45', t_eval=t_eval, args=args)
-        print(path.message)
-        df = pd.DataFrame(path.y.T, columns=['y', 'ks', 'kd', 's', 'h', 'g'])
-
-        self.path = df
-
-        return df
-
-    def _log_step(self, t, values: list, y_args: dict, cap_args: dict,
-                  gamma: float, h_k: float = 10, h_h: float = 10,
-                  ou_process=None):
-        """
-
-        Parameters
-        ----------
-        t           :   float
-        values      :   list
-            values at time t, order: y, ks, kd, s, h, r
-        y_args      :   dict
-            arguments for production, incl. tech0, rho, e, tau_y, dep
-        cap_args    :   dict
-            arguments for capital mkt, incl. saving, depreciation, tau_h, tau_s,
-            c1, c2, beta1, beta2
-        gamma       :   float
-        heaviside   :   float
-        ou_process  :   OrnsteinUhlenbeck
-
-        Returns
-        -------
-        velocities  :   list
-        """
-        y, ks, kd, s, h, gamma_mult = values
-
-        if ou_process is not None:
-            news = ou_process.euler_maruyama(t)
-        else:
-            news = 0
-
-        k = kd - (kd - ks) * 0.5 * (1 + np.tanh(h_k * (kd - ks)))
-
-        # Production changes
-        z = y_args['rho'] * k + y_args['e'] * t - y
-        v_y = (np.log(y_args['tech0']) * np.exp(z) - 1) / y_args['tau']
-
-        # Capital supply changes
-        v_ks = (cap_args['saving'] * np.exp(y) - y_args['dep'] * np.exp(ks))
-        v_ks = v_ks / np.exp(ks)
-
-        # Capital demand changes
-        gamma_mult_n = 0.5 * (1 + np.tanh(h_h * (ks - kd)))
-        v_g_s = gamma_mult_n - gamma_mult
-        v_h = (-h + np.tanh(gamma * gamma_mult_n * v_y + news)) / cap_args[
-            'tau_h']
-
-        force_s = cap_args['beta1'] * s + cap_args['beta2'] * h
-        v_s = (-s + np.tanh(force_s)) / cap_args['tau_s']
-
-        v_kd = cap_args['c1'] * v_s + cap_args['c2'] * (s - cap_args['s0'])
-
-        return [v_y, v_ks, v_kd, v_s, v_h, v_g_s]
-
-    def overview(self, start: list, y_args: dict, cap_args: dict,
-                 gamma: float, h_k: float = 1e1, h_h: float = 1e1,
-                 theta: float = 0.2, sigma: float = 1, t_end: float = 1e5,
-                 save: str = ''):
+    def overview(self, start: list, y_args: dict, cap_args: dict, gamma: float,
+                 h_k: float = 1e1, h_h: float = 1e1, theta: float = 0.2,
+                 sigma: float = 1, t_end: float = 1e5,
+                 save: str = '') -> pd.DataFrame:
 
         """ Develop exploratory graphics for a set of parameters
 
@@ -174,7 +70,7 @@ class SolowModel(object):
         args = (y_args, cap_args, gamma, h_k, h_h, ou)
 
         # Generate path of variables
-        path = solve_ivp(self._log_step, t_span=(1, t_end), y0=start,
+        path = solve_ivp(self._step, t_span=(1, t_end), y0=start,
                          method='RK45', t_eval=t_eval, args=args)
         print(path.message)
         df = pd.DataFrame(path.y.T, columns=['y', 'ks', 'kd', 's', 'h', 'g'])
@@ -229,11 +125,6 @@ class SolowModel(object):
         ax_lst[0, 2].set_ylabel("Z")
         ax_lst[0, 2].set_xlim(-1, 1)
 
-        for i in ax_lst.shape[0]:
-            for j in ax_lst.shape[1]:
-                ax_lst[i, j].ticklabel_format(style='sci', axis='x',
-                                              scilimits=(0, 0))
-
         # Starting point information
         texts = ['y0: {:.1f}, '.format(start[0]),
                  's0: {:.1f}, h0: {:.1f}'.format(start[3], start[4]),
@@ -251,7 +142,227 @@ class SolowModel(object):
             plt.savefig(save, bbox_inches='tight')
         plt.show()
 
-        return path
+        return df
+
+    def solve(self, start: list, t0: float = 0, t_end: float = 1e2,
+              stoch: bool = True, theta: float = 0.2, sigma: float = 1.0,
+              y_args: dict = None, cap_args: dict = None) -> pd.DataFrame:
+        """ Iterate through the Solow model to generate a path for output,
+        capital (supply & demand), and sentiment
+
+        Parameters
+        ----------
+        start       :   list
+            initial values, order = y, ks, kd, s, h, r
+        t0, t_end   :   float
+            integration interval
+        stoch       :   bool
+            whether to solve the system with a stochastic parameter
+        theta, sigma:   float
+            decay and variance of the Ornstein-Uhlenbeck process
+        y_args      :   dict
+            (optional) dictionary of production arguments (incl. tech0, rho, e,
+            tau_y, dep)
+        cap_args    :   dict
+            (optional) dictionary of capital markets arguments (incl. saving,
+            tau_h, tau_s, c1, c2, beta1, beta2, gamma, h_h)
+
+        Returns
+        -------
+
+        """
+
+        # Arguments
+        t_eval = np.arange(1, int(t_end) - 1)
+        gamma, h_h = cap_args['gamma'], cap_args['h_h']
+
+        if y_args is None or cap_args is None:
+            ou = self.ou
+        else:
+            ou = OrnsteinUhlenbeck(decay=theta, diffusion=sigma)
+
+        if stoch:
+            args = (y_args, cap_args, gamma, 10, h_h, ou)
+        else:
+            args = (y_args, cap_args, gamma, 10, h_h, None)
+
+        # Generate path of variables
+        path = solve_ivp(self._step, t_span=(1, t_end), y0=start,
+                         method='RK45', t_eval=t_eval, args=args)
+        print(path.message)
+        df = pd.DataFrame(path.y.T, columns=['y', 'ks', 'kd', 's', 'h', 'g'])
+
+        self.path = df
+
+        return df
+
+    def _step(self, t, values: list, y_args: dict, cap_args: dict, gamma: float,
+              h_k: float = 10, h_h: float = 10, ou_process=None):
+        """
+
+        Parameters
+        ----------
+        t           :   float
+        values      :   list
+            values at time t, order: y, ks, kd, s, h, r
+        y_args      :   dict
+            arguments for production, incl. tech0, rho, e, tau_y, dep
+        cap_args    :   dict
+            arguments for capital mkt, incl. saving, depreciation, tau_h, tau_s,
+            c1, c2, beta1, beta2
+        gamma       :   float
+            strength of the feedback effect
+        h_k, h_h   :   float
+            Extent to which tanh will approximate the min() for k and for gamma
+        ou_process  :   OrnsteinUhlenbeck
+
+        Returns
+        -------
+        velocities  :   list
+        """
+        y, ks, kd, s, h, gamma_mult = values
+
+        if ou_process is not None:
+            news = ou_process.euler_maruyama(t)
+        else:
+            news = 0
+
+        k = kd - (kd - ks) * 0.5 * (1 + np.tanh(h_k * (kd - ks)))
+
+        # Production changes
+        z = y_args['rho'] * k + y_args['e'] * t - y
+        v_y = (np.log(y_args['tech0']) * np.exp(z) - 1) / y_args['tau']
+
+        # Capital supply changes
+        v_ks = (cap_args['saving'] * np.exp(y) - y_args['dep'] * np.exp(ks))
+        v_ks = v_ks / np.exp(ks)
+
+        # Capital demand changes
+        gamma_mult_n = 0.5 * (1 + np.tanh(h_h * (ks - kd)))
+        v_g_s = gamma_mult_n - gamma_mult
+        v_h = (-h + np.tanh(gamma * gamma_mult_n * v_y + news)) / cap_args[
+            'tau_h']
+
+        force_s = cap_args['beta1'] * s + cap_args['beta2'] * h
+        v_s = (-s + np.tanh(force_s)) / cap_args['tau_s']
+
+        v_kd = cap_args['c1'] * v_s + cap_args['c2'] * (s - cap_args['s0'])
+
+        return [v_y, v_ks, v_kd, v_s, v_h, v_g_s]
+
+    def recession_timing(self, gdp: pd.Series = None, timescale: float = 63):
+        """ Calculate the start and end of recessions on the basis of gdp growth.
+        Two consecutive periods of length t that have negative growth start a
+        recession. Two with positive growth end it.
+
+        Parameters
+        ----------
+        gdp :   pd.Series
+            Time series of production
+        timescale   :   float (default 63)
+            Timescale in business days over which to evaluate recession, default
+            is quarterly (63 business days)
+
+        Returns
+        -------
+        df  :   pd.DataFrame
+            DataFrame containing full cycles in sample. I.e. start at first
+            moment of expansion, end at last moment of expansions. Gives the
+            expa
+
+        """
+        if gdp is None:
+            assert self.path is not None, "No GDP saved"
+            print("Using saved GDP")
+            gdp = self.path.y
+
+        growth = gdp.iloc[::timescale].pct_change()
+        ix = growth < 0
+
+        # expansion start => negative growth to two consecutive expansions
+        expansions = np.flatnonzero(
+                (ix.shift(-1) == False) & (ix == True) & (ix.shift(1) == True))
+
+        # recession start => positive growth to two consecutive contractions
+        recessions = np.flatnonzero(
+                (ix == True) & (ix.shift(1) == False) & (ix.shift(2) == False))
+
+        # Convert to indexes in original
+        expansions = [growth.index[i] for i in expansions]
+        recessions = [growth.index[i] for i in recessions]
+
+        # Generate start end tuples, first is given (assume we start in exp.)
+        se = [(0, min(recessions))]
+
+        i = 0
+        while i < len(expansions):
+            # Determine the index of the next recession
+            rec_ix = min([ix for ix in recessions if ix > expansions[i]]
+                         + [gdp.shape[0]])
+            # Check if the recession is admissible
+            if rec_ix > se[-1][1]:
+                se.append((expansions[i], rec_ix))
+            # Find expansion after this
+            exp = min([i for i in expansions if i > rec_ix] + [gdp.shape[0]])
+            if exp == gdp.shape[0]:
+                break
+            else:
+                i = expansions.index(exp)
+
+        self.recessions = pd.DataFrame(se, columns=['expansion', 'recession'])
+        return self.recessions
+
+    def recession_analysis(self, rec: pd.DataFrame = None,
+                           gdp: pd.Series = None) -> pd.DataFrame:
+        """ Function to wrap some basic analysis of the recessions
+
+        Parameters
+        ----------
+        rec  :   pd.DataFrame
+            recessions with first col being expansion starts, and second
+            recession starts
+        gdp     :   pd.Series
+
+
+        Returns
+        -------
+        analysis    :   dict
+        """
+        if gdp is None:
+            assert self.path is not None, "No GDP saved"
+            gdp = self.path.y
+
+        if rec is None:
+            if self.recessions is None:
+                print("Determining Recessions...")
+                self.recession_timing(gdp)
+            rec = self.recessions
+
+        mdd, down, up = [], [], []
+
+        # Find the max drawdown and the time to max drawdown
+        for i in range(rec.shape[0] - 1):
+            # expansion to expansion
+            window = gdp.loc[rec.iloc[i, 0]:rec.iloc[i + 1, 0]]
+            # recession to recession
+            window2 = gdp.loc[rec.iloc[i, 1]:rec.iloc[i + 1, 1]]
+            # Maximum drawdown during a cycle
+            mdd.append(100 * (window.max() - window2.min()) / window.max())
+            # Time from start of recession to trough
+            down.append(window2.idxmin() - rec.iloc[i, 1])
+
+        analysis = {
+            'min': np.min(rec.expansion.diff(1)),
+            'mean': np.mean(rec.expansion.diff(1)),
+            'max': np.max(rec.expansion.diff(1)),
+            'std': np.std(rec.expansion.diff(1)),
+            'mdd': np.mean(mdd),
+            'down': np.mean(down)
+        }
+
+        return pd.DataFrame.from_dict(analysis, orient='index')
+
+    """ WORKS IN PROGRESS """
 
     def _critical_points(self, start: list, y_args: dict, cap_args: dict,
                          gamma: float, phi: float):
@@ -272,7 +383,7 @@ class SolowModel(object):
                 (-1, 1), (-1, 1), (-np.inf, np.inf)]
 
         def f(point):
-            v = self._log_step(1, point, *args)
+            v = self._step(1, point, *args)
             return sum([np.sqrt(v[3] ** 2), np.sqrt(v[4] ** 2)])
 
         solutions = []
@@ -452,97 +563,3 @@ class SolowModel(object):
         if save is not '':
             plt.savefig(save, bbox_inches='tight')
         plt.show()
-
-    def recession_timing(self, gdp: pd.Series, timescale=63):
-        """ Calculate the start and end of recessions on the basis of gdp growth.
-        Two consecutive periods of length t that have negative growth start a
-        recession. Two with positive growth end it.
-
-        Parameters
-        ----------
-        gdp
-        t
-
-        Returns
-        -------
-        df  :   pd.DataFrame
-            DataFrame containing full cycles in sample. I.e. start at first
-            moment of expansion, end at last moment of expansions. Gives the
-            expa
-
-        """
-
-        growth = gdp.iloc[::timescale].pct_change()
-        ix = growth < 0
-
-        # expansion start => negative growth to two consecutive expansions
-        expansions = np.flatnonzero(
-                (ix.shift(-1) == False) & (ix == True) & (ix.shift(1) == True))
-
-        # recession start => positive growth to two consecutive contractions
-        recessions = np.flatnonzero(
-                (ix == True) & (ix.shift(1) == False) & (ix.shift(2) == False))
-
-        # Convert to indexes in original
-        expansions = [growth.index[i] for i in expansions]
-        recessions = [growth.index[i] for i in recessions]
-
-        # Generate start end tuples, first is given (assume we start in exp.)
-        se = [(0, min(recessions))]
-
-        i = 0
-        while i < len(expansions):
-            # Determine the index of the next recession
-            rec_ix = min([ix for ix in recessions if ix > expansions[i]]
-                         + [gdp.shape[0]])
-            # Check if the recession is admissible
-            if rec_ix > se[-1][1]:
-                se.append((expansions[i], rec_ix))
-            # Find expansion after this
-            exp = min([i for i in expansions if i > rec_ix] + [gdp.shape[0]])
-            if exp == gdp.shape[0]:
-                break
-            else:
-                i = expansions.index(exp)
-
-        return pd.DataFrame(se, columns=['expansion', 'recession'])
-
-    @staticmethod
-    def recession_analysis(rec, gdp) -> dict:
-        """ Function to wrap some basic analysis of the recessions
-
-        Parameters
-        ----------
-        rec  :   pd.DataFrame
-            recessions with first col being expansion starts, and second
-            recession starts
-        gdp     :   pd.Series
-
-
-        Returns
-        -------
-        analysis    :   dict
-        """
-        mdd, down, up = [], [], []
-
-        # Find the max drawdown and the time to max drawdown
-        for i in range(rec.shape[0] - 1):
-            # expansion to expansion
-            window = gdp.loc[rec.iloc[i, 0]:rec.iloc[i + 1, 0]]
-            # recession to recession
-            window2 = gdp.loc[rec.iloc[i, 1]:rec.iloc[i + 1, 1]]
-            # Maximum drawdown during a cycle
-            mdd.append(100 * (window.max() - window2.min()) / window.max())
-            # Time from start of recession to trough
-            down.append(window2.idxmin() - rec.iloc[i, 1])
-
-        analysis = {
-            'min': np.min(rec.expansion.diff(1)),
-            'mean': np.mean(rec.expansion.diff(1)),
-            'max': np.max(rec.expansion.diff(1)),
-            'std': np.std(rec.expansion.diff(1)),
-            'mdd': np.mean(mdd),
-            'down': np.mean(down)
-        }
-
-        return analysis
