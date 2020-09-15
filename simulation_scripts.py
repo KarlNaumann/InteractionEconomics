975ce643898b50d3b942ee6c70b426da8524a61e
KarlNaumann <karl.naumann@icloud.com>
2020-09-15 13:50:39 +0200

    Added directory parsing

--- simulation_scripts.py
+++ simulation_scripts.py
@@ -1,4 +1,8 @@
+import os
+import pickle
+
 import numpy as np
+import pandas as pd
 
 from solowModel import SolowModel
 
@@ -40,10 +44,32 @@ def sim_models(parameters: dict, initial_values: np.ndarray,
     for i, seed in enumerate(seeds):
         if verbose:
             print("Seed {} ({}/{})".format(seed, i, len(seeds)))
-        _ = sm.solve(initial_values, t_end, seed=seed, case=case, save=True,
-                     folder=save_loc)
+        _ = sm.solve(initial_values, t_end, seed=seed, case=case, save=False)
+        sm.save_model(folder=save_loc)
 
     return None
+
+
+def directory_dataframe(save_loc: str = 'pickles/'):
+    file_list = os.listdir(save_loc)
+
+    cols = ['t_end', 'gamma', 'epsilon', 'c1', 'c2', 'beta1', 'beta2', 'tau_y',
+            'tau_s',
+            'tau_h', 'saving', 'seed', 'file']
+    directory = pd.DataFrame(index=list(range(len(file_list))), columns=cols)
+
+    for i, file in enumerate(file_list):
+        temp = file.split('_')
+        directory.iloc[i, :] = [float(temp[1][1:]), float(temp[2][1:]),
+                                float(temp[3][1:]), float(temp[5]),
+                                float(temp[7]), float(temp[9]), float(temp[11]),
+                                int(temp[12][2:]), int(temp[13][2:]),
+                                int(temp[14][2:]), float(temp[15][3:]),
+                                int(temp[16][4:6]), file]
+
+    save_file = open(save_loc + 'directory.df', 'wb')
+    pickle.dump(directory, save_file)
+    save_file.close()
 
 
 if __name__ == '__main__':
@@ -58,5 +84,19 @@ if __name__ == '__main__':
     # Accurate production adjustment
     start[0] = params['epsilon'] + params['rho'] * min(start[1:3])
 
-    sim_models(params, start, seeds=list(range(10)),
+    params['c2'] = 2e-4
+    sim_models(params, start, seeds=list(range(1, 20)),
+               save_loc='long_run_approx/', verbose=True)
+
+    params['c2'] = 4e-4
+    sim_models(params, start, seeds=list(range(1, 20)),
+               save_loc='long_run_approx/', verbose=True)
+
+    params['c2'] = 3e-4
+    params['gamma'] = 1000
+    sim_models(params, start, seeds=list(range(1, 20)),
+               save_loc='long_run_approx/', verbose=True)
+
+    params['gamma'] = 3000
+    sim_models(params, start, seeds=list(range(1, 20)),
                save_loc='long_run_approx/', verbose=True)
